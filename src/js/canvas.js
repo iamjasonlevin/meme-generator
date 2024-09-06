55eff49fc486593f826f6705e7751df22a7355ea
Jason Levin <60948787+iamjasonlevin@users.noreply.github.com>
2024-09-06 13:42:30 +0000

    new meme templates

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -4,10 +4,16 @@ import { MAX_SHADOW_BLUR_SIZE, MAX_STROKE_WIDTH, MAX_ROTATE } from './constants.
 export class Canvas {
   #canvas = null;
   #ctx = null;
+  #isDragging = false;
+  #draggedTextbox = null;
+  #dragStartX = 0;
+  #dragStartY = 0;
+  #eventListenersAdded = false;
 
   constructor(canvasEl) {
     this.#canvas = canvasEl;
     this.#ctx = this.#canvas.getContext('2d');
+    this.#addEventListeners();
   }
 
   get width() {
@@ -44,6 +50,9 @@ export class Canvas {
   }
 
   draw(image, textboxes = new Map()) {
+    this.currentImage = image;
+    this.textboxes = textboxes;
+
     if (image == null) {
       return;
     }
@@ -128,5 +137,100 @@ export class Canvas {
 
   static createInstance(canvasEl) {
     return new Canvas(canvasEl);
+  }
+
+  #addEventListeners() {
+    if (this.#eventListenersAdded) return;
+
+    this.#canvas.addEventListener('touchstart', this.#handleTouchStart.bind(this), {
+      passive: false
+    });
+    this.#canvas.addEventListener('touchmove', this.#handleTouchMove.bind(this), {
+      passive: false
+    });
+    this.#canvas.addEventListener('touchend', this.#handleTouchEnd.bind(this));
+
+    this.#canvas.addEventListener('mousedown', this.#handleMouseDown.bind(this));
+    this.#canvas.addEventListener('mousemove', this.#handleMouseMove.bind(this));
+    this.#canvas.addEventListener('mouseup', this.#handleMouseUp.bind(this));
+
+    this.#eventListenersAdded = true;
+  }
+
+  #handleMouseDown(e) {
+    this.#startDragging(e.clientX, e.clientY);
+  }
+
+  #handleMouseMove(e) {
+    if (this.#isDragging && this.#draggedTextbox) {
+      this.#drag(e.clientX, e.clientY);
+    }
+  }
+
+  #handleMouseUp() {
+    this.#stopDragging();
+  }
+
+  #handleTouchStart(e) {
+    e.preventDefault();
+    const touch = e.touches[0];
+    const rect = this.#canvas.getBoundingClientRect();
+    const touchX = touch.clientX - rect.left;
+    const touchY = touch.clientY - rect.top;
+    this.#startDragging(touchX, touchY);
+  }
+
+  #handleTouchMove(e) {
+    e.preventDefault();
+    if (this.#isDragging && this.#draggedTextbox) {
+      const touch = e.touches[0];
+      const rect = this.#canvas.getBoundingClientRect();
+      const touchX = touch.clientX - rect.left;
+      const touchY = touch.clientY - rect.top;
+      this.#drag(touchX, touchY);
+    }
+  }
+
+  #handleTouchEnd(e) {
+    e.preventDefault();
+    this.#stopDragging();
+  }
+
+  #startDragging(clientX, clientY) {
+    const rect = this.#canvas.getBoundingClientRect();
+    const mouseX = clientX - rect.left;
+    const mouseY = clientY - rect.top;
+
+    this.textboxes.forEach((textbox, id) => {
+      const { data } = textbox;
+      const textX = this.#canvas.width / 2 + data.offsetX;
+      const textY =
+        (this.#ctx.measureText('M').width + data.fontSize / 2) *
+          (Array.from(this.textboxes.keys()).indexOf(id) + 1) +
+        data.offsetY;
+
+      if (Math.abs(mouseX - textX) < 50 && Math.abs(mouseY - textY) < 50) {
+        this.#isDragging = true;
+        this.#draggedTextbox = textbox;
+        this.#dragStartX = mouseX - data.offsetX;
+        this.#dragStartY = mouseY - data.offsetY;
+      }
+    });
+  }
+
+  #drag(clientX, clientY) {
+    const rect = this.#canvas.getBoundingClientRect();
+    const mouseX = clientX - rect.left;
+    const mouseY = clientY - rect.top;
+
+    this.#draggedTextbox.data.offsetX = mouseX - this.#dragStartX;
+    this.#draggedTextbox.data.offsetY = mouseY - this.#dragStartY;
+
+    this.draw(this.currentImage, this.textboxes);
+  }
+
+  #stopDragging() {
+    this.#isDragging = false;
+    this.#draggedTextbox = null;
   }
 }
